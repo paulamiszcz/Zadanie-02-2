257ecef6fb0302a73a14e2cd53a0f43d666cdf64
Paula <paulamiszcz@wp.pl>
2018-05-21 21:00:50 +0200

    Stworzenie klasy CAR do przechowywania pól informacji o samochodzie

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+class Car {
+    String brand, model;
+    int year;
+    double capacity;
+
+    Car(String mark, String mod, int rok, double poj){
+        brand = mark;
+        model = mod;
+        year = rok;
+        capacity = poj;
+    }
+}
